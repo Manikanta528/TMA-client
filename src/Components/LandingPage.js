314d1ae933cc978523fd5ed77cd9b100bf18ff66
Manikanta528 <chmanikanta528@gmail.com>
2022-12-23 21:04:12 +0530

    Features section added

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -30,11 +30,78 @@ export default function LandingPage(){
             <div className="scroll">
                 <div>
                     <AiOutlineArrowDown className="down-arrow"/> 
-                    <span className="scroll-text" style={{color: "#068932"}}>Know more</span>
+                    <span className="scroll-text" >Know more</span>
                     <AiOutlineArrowDown className="down-arrow"/>
                 </div>
             </div>
             </main>
+            <section className="features-section">
+                <div className="ft-text">Features</div>
+                {/* f1 */}
+                <div className="features1">
+                    <div className="ft-info">
+                        <h3 className="ft-info-title">
+                            Organize everything in life
+                        </h3>
+                        <p className="ft-info-subtitle" >
+                            Whether there is a work-related task or a personal goal, TMA is here to help you manage all your to-dos.
+                        </p>
+                        
+                        <a href="#" className="ref-link">
+                            Read More...
+                        </a>
+                    </div>
+                    <div className="ft-info-img ft-img1"></div>
+                </div>
+
+                <div className="features1">
+                    <div className="ft-info-img ft-img2">
+                    </div>
+                    <div className="ft-info">
+                        <h3 className="ft-info-title">
+                            Note Taking
+                        </h3>
+                        <p className="ft-info-subtitle" >
+                            Capture your thoughts and securely access them from any where. Go paperless with TMA.
+                        </p>
+                        <a href="#" className="ref-link">
+                            Read More...
+                        </a>
+                    </div>
+                </div>
+
+                <div className="features1">
+                    <div className="ft-info">
+                        <h3 className="ft-info-title">
+                            Pomodoro Timer
+                        </h3>
+                        <p className="ft-info-subtitle" >
+                        Flexible and easy to use timer. Track time spent on tasks, stay focused, analyze data, and improve productivity.
+                        </p>
+                        
+                        <a href="#" className="ref-link">
+                            Read More...
+                        </a>
+                    </div>
+                    <div className="ft-info-img ft-img3"></div>
+                </div>
+
+                <div className="features1">
+                    <div className="ft-info-img ft-img4">
+                    </div>
+                    <div className="ft-info">
+                        <h3 className="ft-info-title">
+                            Historical Analysis
+                        </h3>
+                        <p className="ft-info-subtitle" >
+                            Time spent on work every day/week/month and the time ratio of your work. Make time traceable.                        </p>
+                        <a href="#" className="ref-link">
+                            Read More...
+                        </a>
+                    </div>
+                </div>
+
+            </section>
         </div>
     );
 }
