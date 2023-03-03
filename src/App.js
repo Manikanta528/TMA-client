153edf643118145444adbe9904663d05d1625f22
Manikanta528 <chmanikanta528@gmail.com>
2023-03-03 06:12:29 +0530

    Routes and components added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import LandingPage from "./Components/LandingPage";
-import Task from "./Components/Task";
 import LoginPage from "./Components/LoginPage";
 import RegisterPage from "./Components/RegisterPage";
+import Task from "./Components/Task";
+import NotFound from "./Components/NotFound";
+import PomodoroTimer from './Components/PomodoroTimer';
+import Note from './Components/Note';
+import Dashboard from './Components/Dashboard';
 
 function App() {
+
   return (
     
     <Routes>
       <Route exact path='/' element={<LandingPage/>}/>
       <Route path='/login' element={<LoginPage/>}/>
       <Route path='/register' element={<RegisterPage/>}/>
+
+      <Route path='/task' element={<Task/>}/>
+      <Route path='/pomodoro' element={<PomodoroTimer/>}/>
+      <Route path='/note' element={<Note/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+
+      <Route path='*' element={<NotFound/>}/>    
     </Routes>
     
   );
